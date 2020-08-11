679a3328772f5bd9fc04d6bc1d72f670088b3bf4
Michael Rabon <nobar86@gmail.com>
2020-08-11 20:36:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,9 +54,10 @@ class Owner
   @pets.each do |type, name_array|
       name_array.each do |pet|
         pet.mood = "nervous"
-      end 
+      end
+      
     end
-    @pets = {}
+   
   
   
 end
